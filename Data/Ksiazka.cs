a535d41f222fb48ab6c71691ee093883e6d2b56d
JacekJochemczyk <j.jochemczyk@gmail.com>
2024-10-06 14:10:13 +0200

    tak

---
+++ Data/Ksiazka.cs
@@ -0,0 +1,11 @@
+﻿namespace Biblioteka.Data
+{
+    public class Ksiazka
+    {
+        public int Id { get; set; }   // Klucz główny, unikalny identyfikator książki
+        public string Tytul { get; set; }   // Tytuł książki
+        public string Autor { get; set; }   // Autor książki
+        public int Rok { get; set; } // Rok wydania ksiazkis
+        public bool Dostepna { get; set; } = true; // Czy książka jest dostępna do wypożyczenia
+    }
+}
